01c525c8d4b89c1adb355e5c56c28baad508c7c0
Jim <herencia.j30@gmail.com>
2023-08-04 13:45:34 -0500

    Creacion de tablas en DB

--- src/main/java/com/proyecto/model/Usuario.java
+++ src/main/java/com/proyecto/model/Usuario.java
@@ -1,12 +1,20 @@
 package com.proyecto.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name= "usuario")
+@Table(name = "usuarios")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String username;
@@ -16,6 +24,11 @@ public class Usuario {
 	private String tipo;
 	private String contra;
 	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario() {
 		// TODO Auto-generated constructor stub
@@ -84,6 +97,12 @@ public class Usuario {
 	}
 	public void setContra(String contra) {
 		this.contra = contra;
+	}
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 
 	@Override
